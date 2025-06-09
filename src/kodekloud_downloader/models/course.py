d757fc7c7c54fbb5381aec5c30100f0ee5036a7b
HamadaBadr <121123801+HamadaBadr@users.noreply.github.com>
2025-06-09 17:19:56 +0300

    Update course.py

--- src/kodekloud_downloader/models/course.py
+++ src/kodekloud_downloader/models/course.py
@@ -40,6 +40,7 @@ class IncludesSection(BaseModel):
     quiz_lessons: bool
     quiz_lesson_count: int
     mock_exams: bool
+    community_support: Optional[bool] = None
     hours_of_video: int
     course_duration: int
 
